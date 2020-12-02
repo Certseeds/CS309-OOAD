e25730fa4270021bdceb25bebea0ff1f80268469
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-12-03 00:11:50 +0800

    Branch: staticFactory, finish staticFactory
    Signed-off-by: Killer_Quinn <51754303+Certseeds@users.noreply.github.com>
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ Factory/staticFactory/ITStaff.java
@@ -0,0 +1,9 @@
+package staticFactory;
+
+public interface ITStaff{
+	public String working();
+	public int getSalary();
+	public String getName();
+	public String getUserID();
+	public String toString();
+}
